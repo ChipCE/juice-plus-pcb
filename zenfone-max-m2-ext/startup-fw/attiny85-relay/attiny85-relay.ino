e38817f7d22aefe9da8da3ab7021bdff6c8e7ec1
ChipCE <login721@gmail.com>
2023-07-17 07:54:04 +0900

    sync

--- zenfone-max-m2-ext/startup-fw/attiny85-relay/attiny85-relay.ino
+++ zenfone-max-m2-ext/startup-fw/attiny85-relay/attiny85-relay.ino
@@ -1,21 +1,37 @@
 #define RELAY_PIN 3
-#define TRIGGER_DELAY 3000
-#define TRIGGER_TIME 500
 
+#define STARTUP_DELAY 3000
+#define TRIGGER_TIME 1000
+#define TRIGGER_DELAY 2000
+#define TRIGGER_COUNT 3
+
+long lastTimestamp;
+int currentTriggerCount;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(RELAY_PIN,OUTPUT);
   digitalWrite(RELAY_PIN,LOW);
-  delay(TRIGGER_DELAY);
+
+  currentTriggerCount = 0;
+  lastTimestamp = millis();
+}
+
+void triggerRelay() {
   digitalWrite(RELAY_PIN,HIGH);
   delay(TRIGGER_TIME);
   digitalWrite(RELAY_PIN,LOW);
-  
-
+  currentTriggerCount++;
+  lastTimestamp = millis();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if (currentTriggerCount == 0) {
+    if (millis() -lastTimestamp > STARTUP_DELAY)
+      triggerRelay();
+  } 
+  else if (currentTriggerCount < TRIGGER_COUNT) {
+    if (millis() -lastTimestamp > TRIGGER_DELAY)
+      triggerRelay();
+  }
 }
